7d170c612154d10eae16543c686ffa03d506d21a
baz1 <bazin.remi@gmail.com>
2022-03-06 14:43:11 -0800

    Skip Pset11

--- pset11/Pset11.v
+++ pset11/Pset11.v
@@ -1,6 +1,16 @@
 Require Import Frap Pset11Sig.
 
 
+(* Takeaway: see the rule HtLoop of hoare_triple:
+ * idea is to separate the heap in a section that no longer matters
+ * (doesn't change / isn't used, and won't prevent the conclusion from staying
+ *  true at the end when broadening back the scope)
+ * and a section that matters to recurse on.
+ *
+ * Skipping this problem that looks quite time-consuming
+ * (there seems to be a lot of home-made definitions)
+ *)
+
 (* Opaque mtree. *)
 (* ^-- Don't forget to mark predicates opaque after you've finished proving 
  * all the key algebraic properties about them, in order for them to work
